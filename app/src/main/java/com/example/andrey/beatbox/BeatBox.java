f24eae603f51cb6d9ac48e35ea19c7a1b5a694ac
Unknown <paradygmex@gmail.com>
2018-02-20 17:39:56 +0300

    test

---
+++ app/src/main/java/com/example/andrey/beatbox/BeatBox.java
@@ -0,0 +1,77 @@
+package com.example.andrey.beatbox;
+
+import android.content.Context;
+import android.content.res.AssetFileDescriptor;
+import android.content.res.AssetManager;
+import android.media.AudioManager;
+import android.media.SoundPool;
+import android.util.Log;
+
+import com.example.andrey.beatbox.databinding.ListItemSoundBinding;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class BeatBox {
+    public static final String TAG = "BeatBox";
+    public static final String SOUNDS_FOLDER = "sample_sounds";
+    public static final int MAX_SOUNDS = 5;
+
+    private AssetManager mAssets;
+    private List<Sound> mSounds = new ArrayList<>();
+    private SoundPool mSoundPool;
+
+    public BeatBox(Context context) {
+        mAssets = context.getAssets();
+        mSoundPool = new SoundPool(MAX_SOUNDS, AudioManager.STREAM_MUSIC, 0);
+        loadSounds();
+    }
+
+    public void release() {
+        mSoundPool.release();
+    }
+
+
+    public void play (Sound sound) {
+        Integer soundId = sound.getSoundId();
+        if (soundId == null) {
+            return;
+        }
+        mSoundPool.play(soundId, 1.0f, 1.0f, 1, 0, 1.0f);
+    }
+
+    private void loadSounds() {
+        String[] soundNames;
+        try {
+            soundNames = mAssets.list(SOUNDS_FOLDER);
+            Log.i(TAG, "Found: " + soundNames.length + " sounds");
+        } catch (IOException e) {
+            Log.e(TAG, "loadSounds: couldnt list assets" + e);
+            return;
+        }
+
+        for (String filename : soundNames) {
+            try {
+
+                String assetPath = SOUNDS_FOLDER + "/" + filename;
+                Sound sound = new Sound(assetPath);
+                load(sound);
+                mSounds.add(sound);
+            } catch (IOException e) {
+                Log.e(TAG, "loadSounds: анлаки ", e);
+            }
+        }
+    }
+
+    private void load(Sound sound) throws IOException {
+        AssetFileDescriptor afd = mAssets.openFd(sound.getAssetPath());
+        int soundId = mSoundPool.load(afd, 1);
+        sound.setSoundId(soundId);
+    }
+
+    public List<Sound> getSounds() {
+        return mSounds;
+    }
+}
---
+++ app/src/main/java/com/example/andrey/beatbox/SoundViewModel.java
@@ -0,0 +1,36 @@
+package com.example.andrey.beatbox;
+
+
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+
+public class SoundViewModel extends BaseObservable {
+    private Sound mSound;
+    private BeatBox mBeatBox;
+
+    public Sound getSound() {
+        return mSound;
+    }
+
+    public BeatBox getBeatBox() {
+        return mBeatBox;
+    }
+
+    public SoundViewModel(BeatBox beatBox) {
+        mBeatBox = beatBox;
+    }
+
+    @Bindable
+    public String getTitle() {
+        return mSound.getName();
+    }
+
+    public void setSound(Sound sound) {
+        mSound = sound;
+        notifyChange();
+    }
+
+    public void onButtonClicked() {
+        mBeatBox.play(mSound);
+    }
+}
